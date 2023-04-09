e56f165dbc03d7049412675251736f5d3198da1d
NoizyDuck <NoizyDuck84@yandex.ru>
2023-04-09 17:05:40 +0300

    CheckStyle

--- main/src/main/java/ru/practicum/main/request/service/RequestService.java
+++ main/src/main/java/ru/practicum/main/request/service/RequestService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import ru.practicum.main.request.dto.RequestDto;
 
 import java.util.List;
+
 @Service
 public interface RequestService {
     List<RequestDto> getRequestByUserId(Long userId);
